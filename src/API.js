ce1f24d842f363bb5d654803087ca81d8ce8f2bf
Andrea Ascari <andrea.ascari@buildo.io>
2017-01-04 11:15:11 +0100

    Created API class.

---
+++ src/API.js
@@ -0,0 +1,49 @@
+// @flow
+import type { Auth } from './types';
+const axios = require('axios');
+
+
+const baseURL = 'https://api.contactlab.it/hub/v1';
+
+/* Internal abstractions */
+
+module.exports = class API {
+
+  workspaceId: string
+  nodeId: string
+  axios: Object
+
+  constructor(auth: Auth) {
+    this.workspaceId = auth.workspaceId;
+    this.nodeId = auth.nodeId;
+    this.axios = axios.create({
+      baseURL,
+      headers: {
+        Authorization: `Bearer ${auth.token}`
+      },
+      transformResponse: [(data: string) => {
+        return data && JSON.parse(data);
+      }]
+    });
+  }
+
+  get = (opts: Object): Promise<Object> => {
+    return this.axios.get(`/workspaces/${this.workspaceId}/${opts.endpoint}`, { params: { nodeId: this.nodeId } })
+      .then(res => res.data);
+  }
+
+  post = (opts: Object): Promise<Object> => {
+    return this.axios.post(`/workspaces/${this.workspaceId}/${opts.endpoint}`, { data: opts.data })
+      .then(res => res.data);
+  }
+
+  put = (opts: Object): Promise<Object> => {
+    return this.axios.put(`/workspaces/${this.workspaceId}/${opts.endpoint}`, { data: opts.data })
+      .then(res => res.data);
+  }
+
+  del = (opts: Object) => {
+    return this.axios.delete(`/workspaces/${this.workspaceId}/${opts.endpoint}`, { params: { nodeId: this.nodeId } })
+      .then(res => res.data);
+  }
+};
