62081fa8127cce340675fb462a83ee9c457e13a3
Francesco Negri <francesconegri@gmail.com>
2017-01-16 12:57:04 +0100

    Rename 'obj' to 'value' for clarity

--- src/utils.js
+++ src/utils.js
@@ -1,22 +1,22 @@
 // @flow
 
 // Recursively remove undefined, null, empty objects and empty arrays
-export const compact = (obj: any): any => {
-  if (obj === null) {
+export const compact = (value: any): any => {
+  if (value === null) {
     return undefined;
   }
 
-  if (Array.isArray(obj)) {
-    const compacted = obj.map(v => compact(v)).filter(
+  if (Array.isArray(value)) {
+    const compacted = value.map(v => compact(v)).filter(
       v => typeof v !== 'undefined'
     );
 
     return compacted.length === 0 ? undefined : compacted;
   }
 
-  if (typeof obj === 'object') {
-    const compacted = Object.keys(obj).reduce((acc, key) => {
-      const nested = compact(obj[key]);
+  if (typeof value === 'object') {
+    const compacted = Object.keys(value).reduce((acc, key) => {
+      const nested = compact(value[key]);
 
       if (typeof nested === 'undefined') {
         return acc;
@@ -31,5 +31,5 @@ export const compact = (obj: any): any => {
     return Object.keys(compacted).length === 0 ? undefined : compacted;
   }
 
-  return obj;
+  return value;
 };
