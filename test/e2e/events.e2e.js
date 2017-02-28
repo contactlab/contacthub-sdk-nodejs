e942d8a55c3350f03b39aeba69c66dee19b7d3c1
Andrea Ascari <andrea.ascari@buildo.io>
2017-02-28 15:03:06 +0100

    Added test for events filtering by context.

--- test/e2e/events.e2e.js
+++ test/e2e/events.e2e.js
@@ -52,27 +52,36 @@ describe('ContactHub', () => {
   });
 
   describe('getEvents', () => {
-    it('retrieves all Events for a Customer using filters', async () => {
+    it('retrieves all Events for a Customer using date range filter', async () => {
       const dateFrom = new Date('2017-02-10');
       const dateTo = new Date('2017-02-20');
-      const filters = {
-        mode: 'ACTIVE',
-        context: 'WEB',
-        dateFrom,
-        dateTo
-      };
+      const filters = { dateFrom, dateTo };
 
       const events = await ch.getEvents(cid, filters);
       expect(events.length > 0).toBe(true);
-
-      const areWebEvents = events.every((v) => v.context === 'WEB');
-      expect(areWebEvents).toBe(true);
 
       const haveRightDate = events.every((v) => {
         const eventDate = new Date(v.date);
         return dateTo >= eventDate && eventDate >= dateFrom;
       });
       expect(haveRightDate).toBe(true);
+    });
+
+    it('retrieves all Events for a customer with "MOBILE" context', async () => {
+      const eventData = {
+        customerId: cid,
+        context: 'MOBILE',
+        type: 'viewedPage',
+        properties: {}
+      };
+
+      await ch.addEvent(eventData);
+
+      const events = await ch.getEvents(cid, { context: 'MOBILE' });
+      expect(events.length > 0).toBe(true);
+
+      const areMobileEvents = events.every(({ context }) => context === 'MOBILE');
+      expect(areMobileEvents).toBe(true);
     });
   });
 
